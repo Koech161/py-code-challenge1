c59060fa1ccfeb0f4b625028db9a46ff029abb35
Abel Koech <koechabel161@gmail.com>
2024-08-31 21:53:33 +0300

    oop class

---
+++ lib/oop.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self,make,model,year):
+        self.make = make
+        self.model = model
+        self.year = year
+    def display_info(self):
+            print(f'make: {self.make}')
+            print(f'Model: {self.model}')
+            print(f'Year:{self.year}')
+my_car =Car('Toyota', 'Corolla', 2020)   
+print(my_car.display_info())         
